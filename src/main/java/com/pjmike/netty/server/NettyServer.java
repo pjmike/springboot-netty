eb191b647559c92dea0189e936e5209afe6cec2d
pjmike <1173447453@qq.com>
2019-11-07 10:37:21 +0800

    update .gitignore

--- src/main/java/com/pjmike/netty/server/NettyServer.java
+++ src/main/java/com/pjmike/netty/server/NettyServer.java
@@ -6,6 +6,8 @@ import io.netty.channel.ChannelOption;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.util.concurrent.Future;
+import io.netty.util.concurrent.GenericFutureListener;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
